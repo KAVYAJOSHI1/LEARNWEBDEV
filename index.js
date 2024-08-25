cc3e64d098a00aa29674e7254cf5468700da2541
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-25 16:22:54 +0530

    a

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+window.alert("I LOVE PIZZA");//comment
+let age;
+let name="KAVYA";
+let student = false;
+age=19;
+age= age*4;
+console.log(age);
+console.log("NAME IS",name);
+console.log(student);
+document.getElementById("p1").innerHTML="HELLO"+name;
+document.getElementById("p2").innerHTML="YOUR AGE IS"+age;
+document.getElementById("p3").innerHTML="ENROLLED" + student;
