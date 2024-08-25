e77010440845e5f5259200b877d185bfa1d3c691
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-25 20:44:24 +0530

    a

--- 7.js
+++ 7.js
@@ -1,1 +1,14 @@
 //counter program
+let count =0;
+document.getElementById("decreasebutton").onclick=function(){
+   count--; 
+   document.getElementById("countlabel").innerHTML=count;
+}
+document.getElementById("resetbutton").onclick=function(){
+    count=0;
+    document.getElementById("countlabel").innerHTML=count;
+}
+document.getElementById("increasebutton").onclick=function(){
+    count++;
+    document.getElementById("countlabel").innerHTML=count;
+}
