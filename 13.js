e0a6d7d53cd97c9e66d13d21aeb96c90c9ab8e8c
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-26 00:25:09 +0530

    a

--- 13.js
+++ 13.js
@@ -1,14 +1,33 @@
 document.getElementById("mybutton").onclick =function(){
     const check=document.getElementById("mycheckbox");
+    const visabtn=document.getElementById("visabtn");
+    const mastercardbtn=document.getElementById("mastercardbtn");
+    const paypalbtn=document.getElementById("paypalbtn")
     if(check.checked )
         {
 
         console.log("SUBSCRIBED");
-
+        if (visabtn.checked)
+            {
+                console.log("YOU ARE PAYING WITH VISA");
+            }
+           else if (mastercardbtn.checked)
+            {
+                console.log("YOU ARE PAYING WITH  MASTERCARD");
+            }
+           else  if (paypalbtn.checked)
+            {
+                console.log("YOU ARE PAYING WITH PAYPAL");
+            }
+            else
+            {
+                console.log("PLEASE SELECT PAYMENT METHOD");
+            }
     }
     else
     {
 
         console.log("NOT SUBSCRIBED");
     }
+   
 }
