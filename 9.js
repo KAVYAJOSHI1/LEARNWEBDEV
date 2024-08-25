827e47161f2a673b6a8f266461dcfb9d689d01fe
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-25 23:23:49 +0530

    a

--- 9.js
+++ 9.js
@@ -1,3 +1,12 @@
 //usefull string properties aand methids
 let name="kavya";
-console.log(username.length);
+let number="12345-5-1";
+console.log(name.length);
+console.log(name.charAt(4));
+console.log(name.indexOf("a"));
+console.log(name.lastIndexOf("a"));
+name=name.trim();
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(number.replaceAll("-","4"));
+
