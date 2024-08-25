23005d61542480e77130f99f822917e0a09edab3
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-26 00:04:13 +0530

    a

---
+++ 13.js
@@ -0,0 +1,13 @@
+document.getElementById("mybutton")==function(){
+    
+    if(document.getElementById("mycheckbox").checked ){
+
+        console.log("SUBSCRIBED");
+
+    }
+    else
+    {
+
+        console.log("NOT SUBSCRIBED");
+    }
+}
