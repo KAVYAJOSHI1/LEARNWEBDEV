28ec49fd1da8d806ee51639a024cbc42fc00e2db
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-25 17:46:02 +0530

    a

--- 4circle.js
+++ 4circle.js
@@ -1,7 +1,19 @@
-let pi=3.14159;
+const pi=3.14159;
 let radius;
 let circumference;
 radius=window.prompt("Enter the radius of the circle");
 radius=Number(radius);
 circumference=2*pi*radius;
 console.log("CIRCUMFERENCE OF THE CIRCLE IS",circumference);
+
+//to use math funciton
+x=5.26;
+x=Math.round(x);
+console.log(x);
+x=Math.pow(x,3);
+console.log(x);
+x=Math.sqrt(x);
+console.log(x)
+//Math.max(a,s,d);
+//Math.min(a,s,d);
+//x=Math.PI;
---
+++ 5maths.js
@@ -0,0 +1,5 @@
+//TO find hypotenuse of a triangle
+let a=5;
+let b=6;
+let c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+console.log(h)
