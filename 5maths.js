c8e57a0e1fa92769a4ad48d1739a20b6fe9e729f
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-25 17:50:33 +0530

    a

--- 5maths.js
+++ 5maths.js
@@ -1,5 +1,9 @@
 //TO find hypotenuse of a triangle
-let a=5;
-let b=6;
+let a;
+let b;
+a=window.prompt("ENTER A");
+b=window.prompt("ENTER B");
+a=Number(a);
+b=Number(b);
 let c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
-console.log(h)
+console.log("HYPOTENUSE IS: ",c);
