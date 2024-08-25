3f8112ccfd8f02ba5c30f8711ceefa6ad43b323c
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-25 17:37:57 +0530

    a

--- 3typeconv.js
+++ 3typeconv.js
@@ -3,3 +3,6 @@ age=Number(age);
 console.log(typeof age);
 age=age+1;
 console.log("Happy Birthday ! you are ",age, "years old");
+//type conversion
+x=Number("5")
+console.log(x, typeof x);
---
+++ 4circle.js
@@ -0,0 +1,7 @@
+let pi=3.14159;
+let radius;
+let circumference;
+radius=window.prompt("Enter the radius of the circle");
+radius=Number(radius);
+circumference=2*pi*radius;
+console.log("CIRCUMFERENCE OF THE CIRCLE IS",circumference);
