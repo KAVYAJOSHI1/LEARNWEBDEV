56276891df8d0c7d29c3f957033ef2e7e705cf12
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-26 14:55:12 +0530

    a

---
+++ 29.js
@@ -0,0 +1,13 @@
+//2D arrays
+
+let fruit= ["apple","mango","banana"];
+let vegetable=["potato","onion","brinjal"];
+let meats=["chicken","beef","lamb"];
+let food=[fruit,vegetable,meats];
+for(let list of food)
+{
+    for(let food of list)
+    {
+    console.log(list);
+    }
+}
