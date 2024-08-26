122bc675c8121fc9b50841b7b0b5565bf071569b
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-26 14:42:15 +0530

    a

--- 26.js
+++ 26.js
@@ -9,4 +9,10 @@ console.log(fruit);
 fruit.pop();//remove an element
 fruit.unshift("mango");//add element to beginning
 fruit.shift();//remove element from beginning
+let length=fruit.length;
+
+console.log(length);
+let index=fruit.indexOf("mango");
+console.log(index);
+
 console.log(fruit);
