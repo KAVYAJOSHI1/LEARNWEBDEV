c9bc09a997fd44fee46610fca349cfca543e913b
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-26 15:02:19 +0530

    a

--- 29.js
+++ 29.js
@@ -4,9 +4,10 @@ let fruit= ["apple","mango","banana"];
 let vegetable=["potato","onion","brinjal"];
 let meats=["chicken","beef","lamb"];
 let food=[fruit,vegetable,meats];
+food[0][0]="orange";
 for(let list of food)
 {
-    for(let food of list)
+    for(let a of list)
     {
     console.log(list);
     }
