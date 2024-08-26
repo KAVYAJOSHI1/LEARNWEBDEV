33f1a75d8128d22126f6bd7e4d8781e13d05cbb7
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-26 14:34:53 +0530

    a

---
+++ 26.js
@@ -0,0 +1,12 @@
+//arrays
+let fruit=["apple","mango","banana"];
+console.log(fruit);
+console.log(fruit[0]);
+fruit[0]="orange";
+console.log(fruit);
+fruit.push("lemon");//add an element
+console.log(fruit);
+fruit.pop();//remove an element
+fruit.unshift("mango");//add element to beginning
+fruit.shift();//remove element from beginning
+console.log(fruit);
