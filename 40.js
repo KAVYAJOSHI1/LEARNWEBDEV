47ba85d532fbe2264964fa59f16bcf3c55ea7e0a
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-26 19:34:11 +0530

    a

---
+++ 40.js
@@ -0,0 +1,12 @@
+//arrow function
+
+/*const greeting = (name) =>{
+    console.log("HELLO ",name);
+}
+greeting("Kavya ");*/
+
+const per= (x,y) =>
+{
+    return x/y *100;
+}
+console.log(per(54,100),"%");
