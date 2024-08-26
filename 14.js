9b58bb2d1b0b64e5e7cc56678b05f1554191f941
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-26 11:18:14 +0530

    a

---
+++ 14.js
@@ -0,0 +1,23 @@
+//while
+let grade="A";
+switch(grade)
+{
+    case "A":
+    console.log("excellent");
+    break;
+    case "B":
+    console.log("very good");
+    break;
+    case "C":
+    console.log("good");
+    break;
+    case "D":
+    console.log("fair");
+    break;
+    case "E":
+    console.log("poor");
+    break;
+    default:
+    console.log("invalid");
+
+}
