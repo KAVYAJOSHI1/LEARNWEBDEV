f897b3a3648e29d67f93f0c83a4604f16f8c259f
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-27 11:37:20 +0530

    a

---
+++ 63.js
@@ -0,0 +1,8 @@
+import{PI,getCircumference,getArea} from "./mathutility63.js";
+console.log(PI);
+
+let circumference=getCircumference(5);
+console.log(circumference);
+
+let area = getArea(5);
+console.log(area);
---
+++ mathutility63.js
@@ -0,0 +1,10 @@
+//math utility
+export const PI=3.15159;
+ export function getCircumference(radius)
+{
+    return 2*PI*radius;
+}
+export function getArea(radius)
+{
+    return PI*radius*radius;
+}
