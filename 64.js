4c4820697026c33b6556fb8e82fe80a5a87880a7
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-27 11:44:56 +0530

    a

---
+++ 64.js
@@ -0,0 +1,9 @@
+//DOM =DOCUMENT OBJECT MODEL(API)
+console.dir(document);
+console.log(document.title);
+console.log(document.URL);
+document.title="KAVYA JOSHI";
+//document.location="https://www.google.com/";
+document.body.style.backgroundColor="red";
+document.getElementById("mylabel").innerHTML="HELLO WORLD";
+
