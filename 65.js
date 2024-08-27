49734efbbbebf9837cb690b2129f4ed1670ffcd3
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-27 14:54:03 +0530

    a

---
+++ 65.js
@@ -0,0 +1,17 @@
+/*let element=document.getElementById("mytitle");
+element.style.backgroundColor="blue";
+
+let fruits=document.getElementsByName("fruits");
+fruits.forEach(fruit=>{ if(fruit.checked){console.log(fruit.value);}})
+*/
+
+/*let vegetables=document.getElementsByTagName("li");
+
+vegetables[0].style.backgroundColor="red";
+*/
+
+/*let deserts=document.getElementsByClassName("deserts");
+deserts[0].style.backgroundColor="red";*/
+
+let element=document.querySelector("#mytitle");
+element.style.backgroundColor="blue";
