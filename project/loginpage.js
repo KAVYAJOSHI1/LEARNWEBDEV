23bfccd641c449acdfc1c61fd9493d18d9c6001c
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-31 20:21:04 +0530

    a

--- project/loginpage.js
+++ project/loginpage.js
@@ -7,7 +7,8 @@ let submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", function() {
     if (username.value === "kavya" && password.value === "kavya123") {
         alert("Login successful");
-        window.location.href = "./project/selectionpage.html";
+        window.location.href = "project/selectionpage.html";
+
     } else {
         alert("Login failed");
     }
