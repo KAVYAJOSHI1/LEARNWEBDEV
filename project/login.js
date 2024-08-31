fe3d8fd09ac80ff9a326954c3d7433dd4e3a4a0e
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-31 20:45:51 +0530

    a

--- project/login.js
+++ project/login.js
@@ -5,7 +5,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     let password = document.getElementById("password").value;
 
     if (username === "kavya" && password === "kavya123") {
-        window.location.href = "selectionpage.html"; // Adjust the path based on your structure
+        window.location.href = "slectionpage.html"; // Adjust the path based on your structure
     } else {
         alert("Login failed. Please check your username and password.");
     }
