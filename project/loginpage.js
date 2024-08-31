42d352fa44c9ee87a386bf0aeb15a44596901866
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-31 15:25:52 +0530

    a

---
+++ project/loginpage.js
@@ -0,0 +1,14 @@
+// Correct IDs in the JavaScript
+let username = document.getElementById("name");
+let password = document.getElementById("password");
+let submitButton = document.getElementById("submit");
+
+// Add event listener to the submit button
+submitButton.addEventListener("click", function() {
+    if (username.value === "kavya" && password.value === "kavya123") {
+        alert("Login successful");
+        window.location.href = 'loginpage.html';
+    } else {
+        alert("Login failed");
+    }
+});
