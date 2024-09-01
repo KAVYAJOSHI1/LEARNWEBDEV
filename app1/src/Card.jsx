d313acf553a1b2c498d5995c2cd594d3876993c2
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-01 23:14:29 +0530

    a

---
+++ app1/src/Card.jsx
@@ -0,0 +1,13 @@
+function Card(){
+return(
+<div className="card">
+    <img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png" alt="profile picture"></img>
+    <h2>FLASH TUBE</h2>
+    <p>I am a student, i do coding</p>
+</div>
+
+);
+
+}
+
+export default Card
