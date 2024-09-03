e7e197d1a1e92975a97dc9097c13d9375c21fd28
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-03 11:11:20 +0530

    a

--- app1/src/Card.jsx
+++ app1/src/Card.jsx
@@ -1,9 +1,11 @@
+//add an image to the card from google
+import profilepic from './profile.jpg';
 function Card(){
 return(
 <div className="card">
-    <img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png" alt="profile picture"></img>
-    <h2>FLASH TUBE</h2>
-    <p>I am a student, i do coding</p>
+    <img className='card.image' src={profilepic} alt="profile picture"></img>
+    <h2 className="card-title">FLASH TUBE</h2>
+    <p className="card-text">I am a student, i do coding</p>
 </div>
 
 );
