c0c9f1dbe20e8848e0713073a0bba0173f9ba05c
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-05 11:41:17 +0530

    A

--- app1/src/Card.jsx
+++ app1/src/Card.jsx
@@ -1,15 +1,13 @@
-//add an image to the card from google
-import profilepic from './profile.jpg';
-function Card(){
-return(
-<div className="card">
-    <img className='card.image' src={profilepic} alt="profile picture"></img>
-    <h2 className="card-title">FLASH TUBE</h2>
-    <p className="card-text">I am a student, i do coding</p>
-</div>
+import passportphoto from './passportphoto.jpeg';
 
-);
-
+function Card() {
+  return (
+    <div className="card">
+      <img className='card-image' src={passportphoto} alt="profile picture" />
+      <h2 className="card-title">KAVYA JOSHI</h2>
+      <p className="card-text">I am a student, I do coding</p>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
