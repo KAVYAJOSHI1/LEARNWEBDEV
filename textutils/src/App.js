b1e97365748fcfe38c2234044ecbf3e1ffe27605
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-08 22:31:46 +0530

    a

---
+++ textutils/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+let name="Kavya"
+function App() {
+  return (
+    <>
+
+   <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>COntact</li>
+   </nav>
+   <div className="container">
+    <h1>Hello {name}</h1>
+    <p>HELLO IM HERE</p>
+   </div>
+     </>
+  );
+}
+
+export default App;
